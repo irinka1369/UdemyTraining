4ed898587283a9cef20ea00f56cb361f9512c483
Iryna Sysiak <irinka1369@gmail.com>
2019-10-21 14:55:30 +0300

    add new functions

--- script/js.js
+++ script/js.js
@@ -1,5 +1,13 @@
-let money = +prompt('What is your month budget?', '');
-let time = prompt('Please enter date in this format YYYY-MM-DD', '');
+let money, time;
+function start() {
+  money = +prompt('What is your month budget?', '');
+  time = prompt('Please enter date in this format YYYY-MM-DD', '');
+   
+  while (isNaN(money) || money == '' || money == null) {
+    money = +prompt('What is your month budget?', '');
+  }
+}
+start();
 
 let appData = {
     budget: money,
@@ -8,31 +16,63 @@ let appData = {
            },
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
-for (let i=0; i<2;i++) {
-    let a = prompt('Please write necessary expenses in this month', ''),
-        b = prompt('How much it will cost?', '');
-if( (typeof(a)) === 'string' && (typeof(a) != null) && (typeof(b) != null)&& a !="" && b != "" && a.length < 50)
- {
-     console.log('Done');
-    appData.expenses[a] = b;
-} else {
-        i = i-1;
+
+function chooseExpenses () {
+     for (let i=0; i<2;i++) {
+        let a = prompt('Please write necessary expenses in this month', ''),
+            b = prompt('How much it will cost?', '');
+    if( (typeof(a)) === 'string' && (typeof(a) != null) && (typeof(b) != null)&& a !="" && b != "" && a.length < 50)
+     {
+         console.log('Done');
+        appData.expenses[a] = b;
+    } else {
+            i = i-1;
+    }
+           
+    }
 }
-       
+chooseExpenses();
+
+function detectDayBudget () {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert( 'Everyday budget: ' + appData.moneyPerDay);
 }
 
-appData.moneyPerDay = appData.budget / 30;
+detectDayBudget();
 
-alert( 'Everyday budget: ' + appData.moneyPerDay);
-
-if(appData.moneyPerDay < 100) {
-    console.log('It is minimun budget per day' );
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log('It is medium budget per day');
-} else if (appData.moneyPerDay > 2000) {
-console.log('It is high budget per day');
-}else {
-    console.log('There is some mistake');
+function detectLevel () {
+    if(appData.moneyPerDay < 100) {
+        console.log('It is minimun budget per day' );
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log('It is medium budget per day');
+    } else if (appData.moneyPerDay > 2000) {
+    console.log('It is high budget per day');
+    }else {
+        console.log('There is some mistake');
+    }
 }
+detectLevel();
+
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt('What is your amount of savings?'),
+            percent = +prompt('What is your percent rate?');
+
+            appData.monthIncome = save/100/12*percent;
+    alert('Your month income from deposit is: ' + appData.monthIncome);
+    }
+    
+}
+checkSavings();
+
+function chooseOptExpenses() {
+    for (let i=0; i<3;i++) {
+    let opt = prompt('What are your unnecessary expenses this month?', '');
+    appData.optionalExpenses[i] = opt;
+} 
+
+}
+chooseOptExpenses();
